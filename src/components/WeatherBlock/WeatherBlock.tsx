f87b36aa00ef7acbab8d485cae65cb851cd0276e
Valeria Rybakova <valerian.rybakova@yandex.ru>
2024-06-26 16:35:14 +0300

    feat: добавить в блоке погоды проверку на наличие данных о погоде

--- src/components/WeatherBlock/WeatherBlock.tsx
+++ src/components/WeatherBlock/WeatherBlock.tsx
@@ -6,40 +6,48 @@ function WeatherBlock({ weather }: WeatherBlockProps) {
   return (
     <main>
       <section>
-        <div>
-          <h1>
-            {weather?.city}, {weather?.country}
-          </h1>
-        </div>
-        <div>
-          <img src={weatherIconSrc} alt="" />
+        {!weather ? ( 
           <div>
-            <div>
-              <h3>{weather?.tempMin}&deg;C</h3>
-              <h2>{weather?.temp}&deg;C</h2>
-              <h3>{weather?.tempMax}&deg;C</h3>
-            </div>
-            <div>
-              <p>{weather?.weather}</p>
-            </div>
+            <h1>Нет данных</h1>
           </div>
-        </div>
-        <div>
-          <ul>
-            <li>
-              <p>Pressure</p>
-              <p>{weather?.pressure}</p>
-            </li>
-            <li>
-              <p>Wind</p>
-              <p>{weather?.windSpeed}m/s</p>
-            </li>
-            <li>
-              <p>Humidy</p>
-              <p>{weather?.humidity}%</p>
-            </li>
-          </ul>
-        </div>
+        ) : (
+          <>
+            <div>
+              <h1>
+                {weather?.city}, {weather?.country}
+              </h1>
+            </div>
+            <div>
+              <img src={weatherIconSrc} alt="" />
+              <div>
+                <div>
+                  <h3>{weather?.tempMin}&deg;C</h3>
+                  <h2>{weather?.temp}&deg;C</h2>
+                  <h3>{weather?.tempMax}&deg;C</h3>
+                </div>
+                <div>
+                  <p>{weather?.weather}</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <ul>
+                <li>
+                  <p>Pressure</p>
+                  <p>{weather?.pressure}</p>
+                </li>
+                <li>
+                  <p>Wind</p>
+                  <p>{weather?.windSpeed}m/s</p>
+                </li>
+                <li>
+                  <p>Humidy</p>
+                  <p>{weather?.humidity}%</p>
+                </li>
+              </ul>
+            </div>
+          </>
+        )}
       </section>
     </main>
   );
