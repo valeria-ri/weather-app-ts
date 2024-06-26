a5f12707f2dec4db3a782cc6aef716a1298e678f
Valeria Rybakova <valerian.rybakova@yandex.ru>
2024-06-26 23:31:07 +0300

    fix: округлить числовые значения данных о погоде

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,19 +1,19 @@
 import { WeatherData } from "../types/weatherData"
 
-const pressureFormat = (pressure: number) => Math.floor(pressure * 0.750062);
+const pressureFormat = (pressure: number) => Math.round(pressure * 0.750062);
 
 export const weatherFormat = (data: WeatherData) => {
   return {
     city: data.name,
     country: data.sys.country,
-    temp: data.main.temp,
-    tempMin: data.main.temp_min,
-    tempMax: data.main.temp_max,
-    humidity: data.main.humidity,
+    temp: Math.round(data.main.temp),
+    tempMin: Math.round(data.main.temp_min),
+    tempMax: Math.round(data.main.temp_max),
+    humidity: Math.round(data.main.humidity),
     pressure: pressureFormat(data.main.pressure),
     weather: data.weather[0].description,
     icon: data.weather[0].icon,
-    windSpeed: data.wind.speed,
+    windSpeed: Math.round(data.wind.speed),
     windDeg: data.wind.deg,
   }
 };
