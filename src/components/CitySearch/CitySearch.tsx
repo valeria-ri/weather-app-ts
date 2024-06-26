abfc8db170efd42e9b2d9d084020383fdcf68a7a
Valeria Rybakova <valerian.rybakova@yandex.ru>
2024-06-26 18:50:40 +0300

    feat: настроить монтирование и размонтирование списка найденных городов в зависимости от фокуса инпута

--- src/components/CitySearch/CitySearch.tsx
+++ src/components/CitySearch/CitySearch.tsx
@@ -4,17 +4,18 @@ import { TYPED_CITIES } from "../../utils/constants";
 import { CitySearchProps } from "./types";
 
 function CitySearch({ cityOnClick }: CitySearchProps) {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState<string>("");
   const [filteredCities, setFilteredCities] = useState<City[]>([]);
+  const [isInputFocused, setIsInputFocused] = useState<boolean>(false);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setQuery(value);
 
     if (value) {
-      const matchingCities = TYPED_CITIES
-        .filter((city: City) => city.name.toLowerCase().includes(query.toLowerCase()))
-        .slice(0, 5);
+      const matchingCities = TYPED_CITIES.filter((city: City) =>
+        city.name.toLowerCase().includes(query.toLowerCase())
+      ).slice(0, 5);
       setFilteredCities(matchingCities);
     } else {
       setFilteredCities([]);
@@ -23,27 +24,37 @@ function CitySearch({ cityOnClick }: CitySearchProps) {
 
   const handleSelectCity = (cityId: number) => {
     cityOnClick(cityId);
-    setQuery('');
+    setQuery("");
     setFilteredCities([]);
   };
 
+  const handleInputFocus = () => setIsInputFocused(true);
+
+  const handleInputBlur = () => setIsInputFocused(false);
+
   return (
     <div>
-      <input 
-        type="text" 
-        name="city" 
-        value={query || ''} 
-        onChange={handleInputChange} 
+      <input
+        placeholder="City..."
+        type="text"
+        name="city"
+        value={query || ""}
+        onChange={handleInputChange}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
       />
-      <ul>
-        {
-          filteredCities.map((city, index) => (
-            <li key={index} onClick={() => handleSelectCity(city.id)}>{city.name}, {city.country}</li>
-          ))
-        }
-      </ul>
+      {isInputFocused && query && (
+        <ul>
+          {filteredCities.map((city, index) => (
+            <li key={index} onClick={() => handleSelectCity(city.id)}>
+              <p>{city.name}</p>
+              <p>{city.country}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
-  )
+  );
 }
 
 export default CitySearch;
